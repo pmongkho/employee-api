045785c087a679253eca96e198b69c99b7971fbc
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2023-11-20 12:26:43 -0600

    update .gitmodules

--- obj/Debug/net8.0/EmployeeApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/EmployeeApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc516b50076d7e143d1c38c4b640ada1e5e869f0")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
